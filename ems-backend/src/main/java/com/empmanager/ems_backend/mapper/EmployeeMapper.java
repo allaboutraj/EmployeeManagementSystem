b2dbda56afa32a835ee725c30326ad74679eed40
allaboutraj <rajgupta9507321@gmail.com>
2024-07-27 14:57:01 +0530

    Added Mapper Class

---
+++ ems-backend/src/main/java/com/empmanager/ems_backend/mapper/EmployeeMapper.java
@@ -0,0 +1,25 @@
+package com.empmanager.ems_backend.mapper;
+
+import com.empmanager.ems_backend.dto.EmployeeDto;
+import com.empmanager.ems_backend.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail(),
+                employee.getPhone());
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto) {
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail(),
+                employeeDto.getPhone());
+    }
+}
