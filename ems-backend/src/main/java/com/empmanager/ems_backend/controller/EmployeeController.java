dd88f3def0c8335e1be6b3c8f1f950bca8185936
allaboutraj <rajgupta9507321@gmail.com>
2024-09-18 23:54:45 +0530

    Changed the POrt number In application.Properties file

--- ems-backend/src/main/java/com/empmanager/ems_backend/controller/EmployeeController.java
+++ ems-backend/src/main/java/com/empmanager/ems_backend/controller/EmployeeController.java
@@ -14,12 +14,14 @@ import com.empmanager.ems_backend.service.EmployeeService;
 
 import lombok.AllArgsConstructor;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 
+@CrossOrigin("*")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/employees")
