93e71c75c022228e0e21a1eb5de22c278645ffde
allaboutraj <rajgupta9507321@gmail.com>
2024-09-18 23:53:28 +0530

    Using REST API to populate data through axios

--- ems-frontend/src/components/ListEmployeeComponent.jsx
+++ ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -1,33 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { listEmployees } from "../services/EmployeeService";
 
 const ListEmployeeComponent = () => {
-  const dummyData = [
-    {
-      id: 1,
-      firstName: "John",
-      lastName: "khlo",
-      email: "john@example.com",
-      phone: 123456789,
-    },
-    {
-      id: 2,
-      firstName: "Raj",
-      lastName: "khlo",
-      email: "john@example.com",
-      phone: 123456789,
-    },
-    {
-      id: 3,
-      firstName: "Kumar",
-      lastName: "khlo",
-      email: "john@example.com",
-      phone: 123456789,
-    },
-  ];
+  // const dummyData = [
+  //   {
+  //     id: 1,
+  //     firstName: "John",
+  //     lastName: "khlo",
+  //     email: "john@example.com",
+  //     phone: 123456789,
+  //   },
+  //   {
+  //     id: 2,
+  //     firstName: "Raj",
+  //     lastName: "khlo",
+  //     email: "john@example.com",
+  //     phone: 123456789,
+  //   },
+  //   {
+  //     id: 3,
+  //     firstName: "Kumar",
+  //     lastName: "khlo",
+  //     email: "john@example.com",
+  //     phone: 123456789,
+  //   },
+  // ];
+
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    listEmployees()
+      .then((response) => {
+        setEmployees(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div className="container">
       <h2 className="text-center">List of Employee</h2>
-      <table className="table table-striped table-bordered table-responsive table-hover">
+      <table className="table table-striped table-bordered table-responsive table-hover table-dark">
         <thead>
           <tr>
             <th>Employee Id</th>
@@ -38,7 +52,7 @@ const ListEmployeeComponent = () => {
           </tr>
         </thead>
         <tbody>
-          {dummyData.map((employee) => (
+          {employees.map((employee) => (
             <tr key={employee.id}>
               <td>{employee.id}</td>
               <td>{employee.firstName}</td>
