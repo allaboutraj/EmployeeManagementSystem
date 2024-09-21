a5d3ce5fb0df890cea98b61c98648a4f39d255e1
allaboutraj <rajgupta9507321@gmail.com>
2024-09-22 00:27:01 +0530

    Added Header and Footer

---
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer className="footer">
+        <span>ALl rights reserved 2024 by Manish</span>
+      </footer>
+    </div>
+  );
+};
+
+export default FooterComponent;
