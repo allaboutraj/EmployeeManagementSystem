42b983f076f39d90370fd2d3af73d3ea0a24bae9
allaboutraj <rajgupta9507321@gmail.com>
2024-09-29 21:42:44 +0530

    added ENV file

--- ems-frontend/src/services/EmployeeService.js
+++ ems-frontend/src/services/EmployeeService.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const REST_API_BASE_URL = "http://localhost:8080/api/employees/";
+// const REST_API_BASE_URL = "http://localhost:8080/api/employees/";
+const REST_API_BASE_URL =
+  import.meta.env.VITE_REST_API_BASE_URL ||
+  "http://localhost:8080/api/employees";
 
 export const listEmployees = () => {
   return axios.get(REST_API_BASE_URL);
